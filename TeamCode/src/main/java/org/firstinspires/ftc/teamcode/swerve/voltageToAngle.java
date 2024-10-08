a2580c9ded94d1893de10c46775b82a1cc490f3e
soggy-cereal1 <polargalaxy.314@gmail.com>
2024-10-08 16:12:24 -0400

    updated some things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/voltageToAngle.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.swerve;
-
-import javafx.util;
-public class voltageToAngle {
-
-
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/voltageToAngleConstants.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.swerve;
+import java.util.HashMap;
+import java.lang.Double;
+public class voltageToAngleConstants {
+    HashMap <Double,Double> fl = new HashMap<>();
+    HashMap <Double,Double> fr = new HashMap<>();
+    HashMap <Double,Double> bl = new HashMap<>();
+    HashMap <Double,Double> br = new HashMap<>();
+
+}
