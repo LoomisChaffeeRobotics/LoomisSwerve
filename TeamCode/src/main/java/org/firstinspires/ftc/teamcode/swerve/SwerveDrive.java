5b456a84ed65eefb2ac79a7ec67537af44f2bd19
soggy-cereal1 <polargalaxy.314@gmail.com>
2024-10-11 09:10:09 -0400

    Create SwerveDrive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/SwerveDrive.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.swerve;
+
+public class SwerveDrive {
+    OptimalAngleCalculator calculator;
+    gamepadToVectors gamepadConverter;
+    voltageToAngleConstants angleConverter;
+    public SwerveDrive (double W, double L, double maxTranslationalSpeed, double maxRotSpeed) {
+
+    }
+
+    public void setDrivePower(double x, double y, double rx) {
+
+    }
+    public void vectorSeparation () {
+
+    }
+
+}
