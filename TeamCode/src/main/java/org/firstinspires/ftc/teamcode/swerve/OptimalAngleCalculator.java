b358b417f8c5370772acaa93604e13ce5c3ab37e
Someone0759 <polargalaxy.314@gmail.com>
2024-10-21 15:01:36 -0400

    Reapply "merge"
    This reverts commit ef7ea9e84ca4ad9b8dedebe50b5b5f9d61f56682.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/OptimalAngleCalculator.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.swerve;
+
+public class OptimalAngleCalculator {
+    public double targetAngle;
+    public boolean requiresReversing;
+
+//    public OptimalAngleCalculator(double currentAngle, double vectorAngle)
+//    {
+//        calculateOptimalAngle(currentAngle, vectorAngle);
+//    }
+    // idt there are any parameters to need to be initialized (faster runtimes) but I might be wrong
+    public void calculateOptimalAngle(double currentAngle, double vectorAngle)
+    {
+        double normalizedCurrentAngle = normalizeAngle(currentAngle);
+        double normalizedVectorAngle = normalizeAngle(vectorAngle);
+        double angleDifference = Math.abs(normalizedCurrentAngle - normalizedVectorAngle);
+        if (angleDifference > 90) {
+            targetAngle = normalizeAngle(normalizedVectorAngle + 180);
+            requiresReversing = true;
+        } else {
+            // Go towards the vector's angle
+            targetAngle = normalizedVectorAngle;
+            requiresReversing = false;
+        }
+    }
+    public double normalizeAngle(double angle) {
+        return (angle % 360);
+    }
+    public double getTargetAngle() {
+        return targetAngle;
+    }
+
+    public boolean requiresReversing() {
+        return requiresReversing;
+    }
+    
+}
