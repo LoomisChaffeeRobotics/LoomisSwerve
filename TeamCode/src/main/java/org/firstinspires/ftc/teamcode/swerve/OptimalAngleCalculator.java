f819e45b8d93f1f431053aaf3e9ecfc0670cd0e3
Someone0759 <polargalaxy.314@gmail.com>
2024-11-09 10:42:21 -0500

    Update OptimalAngleCalculator.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/OptimalAngleCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swerve/OptimalAngleCalculator.java
@@ -12,10 +12,6 @@ public class OptimalAngleCalculator {
 //    }
     // idt there are any parameters to need to be initialized (faster runtimes) but I might be wrong
     public double calculateOptimalAngle(double currentAngle, double vectorAngle, int m) {
-
-//        double angleDifference = Math.abs(normalizedCurrentAngle - normalizedVectorAngle);
-//        double zeroTolerance = 15;
-
         double[] angs = {
                 vectorAngle,
                 vectorAngle + 180,
@@ -29,44 +25,22 @@ public class OptimalAngleCalculator {
             diffs[i] = Math.abs(angs[i] - currentAngle);
         }
 
-        Arrays.sort(diffs);
+        int index = 0;
+        double min = diffs[index];
+        for (int i = 1; i < diffs.length; i++) {
+            if (diffs[i] <=min) {
+                min = diffs[i];
+                index = i;
+            }
+        }
 
-        double smallest = diffs[0];
-        if (smallest == vectorAngle + 180 || smallest == vectorAngle - 180) {
+        if (index % 2 == 1) {
             requiresReversing[m] = true;
         } else {
             requiresReversing[m] = false;
         }
 
-        return smallest; //TODO; THIS IS BAD
-
-//        if (Math.abs(differenceNormal) < Math.abs(differenceAdded) && Math.abs(differenceNormal) < Math.abs(differenceSubtracted)) {
-//            //normal is least
-//            requiresReversing = false;
-//            return normalizedVectorAngle;
-//        } else if (Math.abs(differenceAdded) < Math.abs(differenceNormal) && Math.abs(differenceAdded) < Math.abs(differenceSubtracted)) {
-//            requiresReversing = true;
-//            return addedAngle;
-//        } else if {
-//            requiresReversing = true;
-//            return subtractedAngle;
-//        }
-//        if (Math.abs(normalizedVectorAngle) < zeroTolerance) {
-//            requiresReversing = false;
-//            return normalizedVectorAngle;
-//        } else if (270 > angleDifference && angleDifference > 90 ) {
-//            requiresReversing = true;
-//            return normalizeAngle(normalizedVectorAngle + 180);
-//            // might be the problem if the range is supposed to be 0 to 360
-//            // but sometimes it's -180 + 180 is 0 and 0 + 180 is 180 and 180 + 180 is 360
-//            // which is bigger than reachable
-//        } else if (270 > angleDifference) {
-//            requiresReversing = false;
-//            return normalizeAngle(normalizedVectorAngle + 360);
-//        } else {
-//            requiresReversing = false;
-//            return normalizedVectorAngle;
-//        }
+        return angs[index];
     }
     public boolean requiresReversing(int m) {
         return requiresReversing[m];
